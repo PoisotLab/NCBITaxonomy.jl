c2112dd3d229857fd7c659cf2d6ba8538bc80ef3
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-15 15:00:58 -0500

    🔧 fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,15 @@
 using Documenter, NCBITaxonomy
 
+makedocs(
+    sitename="NCBITaxonomy",
+    authors="Timothée Poisot",
+    modules=[NCBITaxonomy],
+    pages=["Index" => "index.md"]
+)
+
+deploydocs(
+    deps=Deps.pip("pygments", "python-markdown-math"),
+    repo="github.com/EcoJulia/NCBITaxonomy.jl.git",
+    devbranch="master",
+    push_preview=true
+)
