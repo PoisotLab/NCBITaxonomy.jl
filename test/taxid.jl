84ea58559a602e305595fdf322c294918bc618b0
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-15 14:05:45 -0500

    ✔️ update tests for taxid

--- test/taxid.jl
+++ test/taxid.jl
@@ -2,18 +2,23 @@ module TestTaxid
     using Test
     using NCBITaxonomy
 
+    # Strict matching with a scientific name
     bos = taxid("Bos taurus")
-
     @test typeof(bos) == NCBITaxon
     @test bos.name == "Bos taurus"
     @test bos.id == 9913
 
+    # Strict matching with a vernacular name
     cow = taxid("cow")
-
     @test typeof(cow) == NCBITaxon
     @test cow.name == "Bos taurus"
     @test cow.id == 9913
 
-    
+    # Fuzzy matching with a scientific name
+    box = taxid("Box taurus"; fuzzy=true)
+    @test typeof(box) == NCBITaxon
+    @test box.name == "Bos taurus"
+    @test box.id == 9913
+
 
 end
