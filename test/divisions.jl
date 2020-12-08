a6a5a4f74f3205b37f376a390df07945e3cb7d9c
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-12-07 20:45:27 -0500

    ✅ add tests

--- test/divisions.jl
+++ test/divisions.jl
@@ -6,8 +6,12 @@ module TestDivisionFinders
     @test typeof(virusfinder()("Ebolavirus")) <: NCBITaxon
     @test typeof(plantfinder()("Acer")) <: NCBITaxon
     @test isnothing(mammalfinder(false)("Homo"))
+    @test isnothing(mammalfinder(false)("Mus"))
+    @test !isnothing(mammalfinder(true)("Mus"))
+    @test !isnothing(rodentfinder()("Mus"))
     @test isnothing(vertebratefinder(false)("Homo"))
     @test !isnothing(vertebratefinder(true)("Homo"))
+    @test !isnothing(primatefinder()("Homo"))
     @test !isnothing(vertebratefinder(true)("Homo"))
     @test !isnothing(bacteriafinder()("Pseudomonas"))
     @test !isnothing(invertebratefinder()("Lamellodiscus"))
