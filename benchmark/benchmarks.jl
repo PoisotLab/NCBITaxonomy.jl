c1e25182da3435f7d5c90253743d5b3427212126
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-03-03 12:53:57 -0500

    Add files via upload

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,14 +1,59 @@
 using BenchmarkTools
 using NCBITaxonomy
+using AbstractTrees
 
 const SUITE = BenchmarkGroup()
 
-SUITE["name_finders"] = BenchmarkGroup(["namefinding", "utilities"])
-SUITE["name_finders"]["mammals_build"] = @benchmarkable mammalfilter()
+# Construction of name finders
 
+SUITE["name finders"] = BenchmarkGroup(["namefinding", "utilities"])
+
+SUITE["name finders"]["mammals"] = @benchmarkable mammalfilter(inclusive = false)
+
+SUITE["name finders"]["mammals (inclusive)"] =
+    @benchmarkable mammalfilter(inclusive = true)
+
+SUITE["name finders"]["phage"] = @benchmarkable phagefilter(inclusive = true)
+
+# Prepare a mammal filter next
 mf = mammalfilter()
 
-SUITE["taxonsearch"] = BenchmarkGroup(["namefinding", "search"])
-SUITE["taxonsearch"]["from_id"] = @benchmarkable taxon(9823)
-SUITE["taxonsearch"]["from_name"] = @benchmarkable taxon("Sus scrofa")
-SUITE["taxonsearch"]["with_finder"] = @benchmarkable taxon(mf, "Sus scrofa")
+# Ability to locate taxa
+
+SUITE["taxon search"] = BenchmarkGroup(["namefinding", "search"])
+
+SUITE["taxon search"]["by ID"] = @benchmarkable taxon(9823)
+
+SUITE["taxon search"]["by name"] = @benchmarkable taxon("Sus scrofa")
+
+SUITE["taxon search"]["with finder"] = @benchmarkable taxon(mf, "Sus scrofa")
+
+SUITE["taxonsearch"]["lowercase with finder"] =
+    @benchmarkable taxon(mf, "Sus scrofa"; lowercase = true)
+
+SUITE["taxonsearch"]["scientific with finder"] =
+    @benchmarkable taxon(mf, "Sus scrofa"; preferscientific = true)
+
+# Ability to traverse a tree
+
+SUITE["traversal"] = BenchmarkGroup(["search", "tree traversal"])
+
+SUITE["traversal"]["children"] = @benchmarkable AbstractTrees.children(ncbi"Monogenea")
+
+SUITE["traversal"]["parent"] = @benchmarkable AbstractTrees.parent(ncbi"Monogenea")
+
+SUITE["traversal"]["in root"] =
+    @benchmarkable AbstractTrees.parent(ncbi"Lamellodiscus elegans", ncbi"Monogenea")
+
+SUITE["traversal"]["lineage"] = @benchmarkable lineage(ncbi"Lamellodiscus vulgaris")
+
+SUITE["traversal"]["common ancestor (pair)"] =
+    @benchmarkable commonancestor(ncbi"Lamellodiscus", ncbi"Dactylogyrus")
+
+SUITE["traversal"]["common ancestor (array)"] = @benchmarkable commonancestor([
+    ncbi"Lamellodiscus",
+    ncbi"Dactylogyrus",
+    ncbi"Paradiplozoon",
+    ncbi"Diplectanum",
+    ncbi"Echinoplectanum",
+])
