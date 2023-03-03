093bf59624fd2f5d10531f2d5a2f1094f1d7a1a8
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-03-03 12:08:48 -0500

    🟩 intree

--- test/lineages.jl
+++ test/lineages.jl
@@ -1,15 +1,27 @@
 module TestLineages
 
-    using Test
-    using NCBITaxonomy
-    using AbstractTrees
+using Test
+using NCBITaxonomy
+using AbstractTrees
 
-    @test AbstractTrees.parent(ncbi"Lamellodiscus") == ncbi"Diplectanidae"
+@test AbstractTrees.parent(ncbi"Lamellodiscus") == ncbi"Diplectanidae"
 
-    @test AbstractTrees.lineage(ncbi"Lamellodiscus"; stop_at=ncbi"Dactylogyridea") == reverse([ncbi"Lamellodiscus", ncbi"Diplectanidae", ncbi"Dactylogyridea"])
+@test lineage(ncbi"Lamellodiscus"; stop_at = ncbi"Dactylogyridea") ==
+      reverse([ncbi"Lamellodiscus", ncbi"Diplectanidae", ncbi"Dactylogyridea"])
 
-    @test AbstractTrees.children(ncbi"Lamellodiscus") collect(AbstractTrees.Leaves(ncbi"Lamellodiscus"))
+@test AbstractTrees.children(ncbi"Lamellodiscus") ==
+      collect(AbstractTrees.Leaves(ncbi"Lamellodiscus"))
 
-    @test AbstractTrees.intree(ncbi"Lamellodiscus", ncbi"Diplectanidae")
+@test AbstractTrees.intree(ncbi"Lamellodiscus", ncbi"Diplectanidae")
+@test AbstractTrees.intree(ncbi"Lamellodiscus elegans", ncbi"Lamellodiscus")
+
+@test commonancestor(ncbi"Lamellodiscus", ncbi"Dactylogyrus") == ncbi"Dactylogyridea"
+@test commonancestor([
+    ncbi"Lamellodiscus",
+    ncbi"Dactylogyrus",
+    ncbi"Paradiplozoon",
+    ncbi"Diplectanum",
+    ncbi"Echinoplectanum",
+]) == ncbi"Monogenea"
 
 end
