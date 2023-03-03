fc8f70fd77c8b44efab80df9bddc6548880e4d32
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-03-02 19:39:12 -0500

    🧹 linter

--- src/NCBITaxonomy.jl
+++ src/NCBITaxonomy.jl
@@ -45,13 +45,14 @@ select!(nodes_table, Not(:hidden_subtree))
 select!(nodes_table, Not(:comments))
 select!(nodes_table, Not(:embl))
 
-nodes_table = innerjoin(nodes_table, division_table; on=:division_id)
+nodes_table = innerjoin(nodes_table, division_table; on = :division_id)
 select!(nodes_table, Not(:division_id))
 
 names_table = leftjoin(
-    names_table, unique(select(nodes_table, [:tax_id, :rank, :parent_tax_id])); on=:tax_id
+    names_table,
+    unique(select(nodes_table, [:tax_id, :rank, :parent_tax_id]));
+    on = :tax_id,
 )
-
 scinames_table = names_table[findall(names_table.class .== class_scientific_name), :]
 
 include("taxon.jl")
